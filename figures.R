b8d77168ad28b88316668c1377407d9b1444b11a
Bryce Morsky <bryce.morsky@gmail.com>
2019-12-10 18:25:36 -0500

    Updated code, added new files

--- figures.R
+++ figures.R
@@ -1,4 +1,4 @@
-load("~/Desktop/best_protocol_quick.Rda")
+load("/Users/brycemorsky/Documents/Notre Dame/ND paper 2/Data/best_protocol.Rda")
 
 library(ggplot2)
 library(cowplot) 
@@ -6,6 +6,6 @@ theme_set(theme_cowplot())
 library(viridis)
 
 q1 <- ggplot(best_protocol, aes(x=On,y=Off,size=Success,colour=Competition)) + geom_point() + scale_color_viridis()
-q2 <- ggplot(best_protocol, aes(x=Competition,y=Success)) + geom_point()
+q2 <- ggplot(best_protocol, aes(x=Competition,y=Success,colour=On)) + geom_point()
 
 save_plot(plot_grid(q1,q2,ncol=2),filename="~/Desktop/result.png",base_height = 4,base_width = 8)
--- gillespie_avg.jl
+++ gillespie_avg.jl
@@ -2,98 +2,47 @@
 # a time series for the protocol.
 using DiffEqBiological, DifferentialEquations, Plots, Statistics
 
-num_sims = 200
-control = 1 # initial input rate of bacteria
+# Initial conditions
+control = 1.0 # amount of antibiotic when on
+num_sims = 10 # number of sims to average over
 time = 2000 # length of process
-X₀ = 1e3 # initial number of susceptible, n₁
-
-# Stochastic LV chemical reaction system
-# Parameters, times are per hour
+X₀ = 1e8 # initial number of susceptible, n₁
 
 # Parameters
-b₁ = 0.2 # susceptible birth rate
-d₁ = 0.18 # susceptible death rate
-b₂ = 0.195 # resistant birth rate
-d₂ = 0.18 # resistant death rate
-γ₁₁ = 1e-4 # susceptible death rate from other susceptible
-γ₁₂ = 0.1e-4 # susceptible death rate from resistant
-γ₂₁ = 10e-4 # resistant death rate from susceptible
-γ₂₂ = 1e-4 # resistant death rate from other resistant
-α₁ = 0.04 # susceptible death rate from antibiotic
-α₂ = 0.0 # resistant death rate from antibiotic
-μ = 2e-5 # mutation rate
-μₐ = 100*μ # mutation rate cause by antibiotic
-η = 0 #4e-3 # pasmid transfer rate
-init_pop = 1e3 #initial population size
-
-β₁ = 0.2 # susceptible birth rate
-δ₁ = 0.18 # susceptible death rate
-β₂ = 0.1 # resistant birth rate
-δ₂ = 0.09 # resistant death rate
-γ₁₁ = 1e-5 # susceptible death rate from other susceptible
-γ₂₂ = 1e-5 # resistant death rate from other resistant
 α₁ = 0.03 # susceptible death rate from antibiotic
 α₂ = 0.0 # resistant death rate from antibiotic
+β₁ = 0.2 # susceptible birth rate
+β₂ = 0.1 # resistant birth rate
+γ₁₁ = 1e-8 # susceptible death rate from other susceptible
+γ₂₂ = 1e-8 # resistant death rate from other resistant
+δ₁ = 0.18 # susceptible death rate
+δ₂ = 0.09 # resistant death rate
+κ = 10
 μ = 2e-5 # mutation rate
 η = 0.0 # 4e-3 # pasmid transfer rate
 
-
-
-# b₁ = 0.02 # susceptible birth rate
-# d₁ = 0.014 # susceptible death rate
-# b₂ = 0.01 # resistant birth rate
-# d₂ = 0.005 # resistant death rate
-# γ₁₁ = 1e-6 # susceptible death rate from other susceptible
-# γ₁₂ = 1e-6 # susceptible death rate from resistant
-# γ₂₁ = 1e-6 # resistant death rate from susceptible
-# γ₂₂ = 1e-6 # resistant death rate from other resistant
-# α₁ = 0.008 # susceptible death rate from antibiotic
-# α₂ = 0.0015 # resistant death rate from antibiotic
-# μ = 1e-6 # mutation rate
-# μₐ = 10*μ # mutation rate cause by antibiotic
-# η = 0 #4e-3 # pasmid transfer rate
-# init_pop = 1e4 #initial population size
-
-# Initial conditions
-antibiotic_amount = 1.0 # amount of antibiotic when on
-# durOn = 700 # durtation the antibiotic is on
-# durOff = 1500 # duration the antibiotic is off
-
-# b₁ = 0.04
-# d₁ = 0.03
-# b₂ = 0.03
-# d₂ = 0.02
-# γ₁₁ = 1e-4
-# γ₁₂ = 0.88e-4
-# γ₂₁ = 1.136e-4
-# γ₂₂ = 1e-4
-# α₁ = 0.0012
-# α₂ = 0
-# μ = 1.5e-8
-# ζ = 0.0
-# η = 0 #4e-3
-# controlrate = 0
-rates = (b₁, d₁, b₂, d₂, γ₁₁, γ₁₂, γ₂₁, γ₂₂, α₁, α₂, μ, μₐ, η)
+# Stochastic LV chemical reaction system
 LV_model = @reaction_network begin
-    b₁, n₁ → 2n₁ # n₁ birth
-    d₁, n₁ → 0 # n₁ death
-	b₂, n₂ → 2n₂ # n₂ birth
-	d₂, n₂ → 0 # n₂ death
-	γ₁₁, n₁ + n₁ → 0 # self competition of n₁
-	a*γ₂₁+(1-a)*γ₁₂, n₁ + n₂ → n₂ # competition to n₁ from n₂
-	a*γ₁₂+(1-a)*γ₂₁, n₂ + n₁ → n₁ # competition to n₂ from n₁
-	γ₂₂, n₂ + n₂ → 0 # self competition of n₂
+    β₁, n₁ → 2n₁ # n₁ birth
+    δ₁, n₁ → 0 # n₁ death
+	β₂, n₂ → 2n₂ # n₂ birth
+	δ₂, n₂ → 0 # n₂ death
+	γ₁₁, n₁ + n₁ → n₁ # self competition of n₁
+	(a*κ + (1-a)/κ)*γ₁₁, n₁ + n₂ → n₂ # competition to n₁ from n₂
+	(a/κ + (1-a)*κ)*γ₂₂, n₂ + n₁ → n₁ # competition to n₂ from n₁
+	γ₂₂, n₂ + n₂ → n₂ # self competition of n₂
 	α₁, a + n₁ → a # death by antibiotic
 	α₂, a + n₂ → a # death by antibiotic, α₂ < α₁
 	μ, n₁ → n₂ # mutation
 	μ, n₂ → n₁ # mutation
-	μₐ, a + n₁ → a + n₂ # mutation due to stress from the antibiotic
+	10*μ, a + n₁ → a + n₂ # mutation due to stress from the antibiotic
 	η, n₁ + n₂ → 2n₂ # plasmid (resistance) transfer
-end b₁ d₁ b₂ d₂ γ₁₁ γ₁₂ γ₂₁ γ₂₂ α₁ α₂ μ μₐ η
+end β₁ δ₁ β₂ δ₂ γ₁₁ κ γ₂₂ α₁ α₂ μ η
+rates = (β₁, δ₁, β₂, δ₂, γ₁₁, κ, γ₂₂, α₁, α₂, μ, η)
 
 # Solve system for various protocols averaged 100 times
-step_length = 20
-num_steps = 15
+step_length = 10
+num_steps = 10
 period = step_length*num_steps
 function runSim()
 	output_mean = zeros(convert(Int64,num_steps*(num_steps-1)/2),3)
@@ -117,7 +66,7 @@ function runSim()
 				t in (protocolOff)
 			end
 			function conditionStop(u,t,integrator)
-				(u[1]+u[2] == 0) | (u[2] > 50)
+				(u[1]+u[2] == 0) | (u[2] > 500)
 			end
 			function affectOn!(integrator) # effect that occurs when the condition is met
 				#integrator.u[3] += 200
@@ -152,6 +101,16 @@ return output_mean,output_std,time_mean,time_std
 end
 # Sweep the protocols
 output_mean,output_std,time_mean,time_std = runSim()
+result = zeros(num_sims)
+for sim = 1:num_sims
+	prob = DiscreteProblem([X₀; 0; control],(0.0,time),rates)
+	jump_prob = JumpProblem(prob,Direct(),LV_model)
+	sol = solve(jump_prob,FunctionMap())
+	result[sim] = 1 - min(sol[1,end] + sol[2,end], 1.0)
+end
+result = mean(result)
+output_mean[:,3] = output_mean[:,3] - result*ones(convert(Int64,num_steps*(num_steps-1)/2),1)
+
 # Sweep X₀ and μ
 output_muX0_mean = zeros(10^2,3)
 output_muX0_std = zeros(10^2,3)
@@ -172,39 +131,114 @@ time_muX0_std = zeros(10^2,3)
 
 #R code to generate heatmaps for the probability of success of the protocols, the average time to success, and the variances of these
 using RCall
-@rput output_mean output_std time_mean time_std output_muX0_mean output_muX0_std time_muX0_mean time_muX0_std
+@rput output_mean #output_std time_mean time_std output_muX0_mean output_muX0_std time_muX0_mean time_muX0_std
 R"""
 library(ggplot2)
 library(cowplot); theme_set(theme_cowplot())
 library(viridis)
 library(viridisLite)
+library(RColorBrewer)
 
 output_mean <- as.data.frame(output_mean)
-output_std <- as.data.frame(output_std)
-time_mean <- as.data.frame(time_mean)
-time_std <- as.data.frame(time_std)
-output_muX0_mean <- as.data.frame(output_muX0_mean)
-output_muX0_std <- as.data.frame(output_muX0_std)
-time_muX0_mean <- as.data.frame(time_muX0_mean)
-time_muX0_std <- as.data.frame(time_muX0_std)
 
-p <- ggplot() + theme(strip.background=element_blank(),legend.justification = c(1, 0),
-  legend.position = c(1, 0.6),plot.margin = unit(c(0, 0, 1.25, 0), "cm"),legend.title=element_blank()) + scale_x_continuous(expand = c(0, 0)) + scale_y_continuous(expand = c(0, 0)) + guides(fill = guide_colorbar(title.hjust = 0.5, title.position = "top"))
+p <- ggplot() + geom_raster(data=output_mean,aes(x=factor(V1),y=factor(V2),fill=V3)) + scale_fill_viridis(limits=c(-1,1)) + ggtitle("Mean success") + labs(x = "On", y = "Off", fill = "Prob. of success") + theme(legend.title.align=0.5,legend.position=c(.7,.8))
 
-p_mean <- p + geom_raster(data=output_mean,aes(x=V1,y=V2,fill=V3)) + scale_fill_viridis(option="viridis",limits = c(0,1)) + labs(x = "On", y = "Off") + ggtitle("Mean")
-p_std <- p + geom_raster(data=output_std,aes(x=V1,y=V2,fill=V3)) + scale_fill_viridis(option="viridis") + labs(x = "On", y = "Off") + ggtitle("SD")
-
-pt_mean <- p + geom_raster(data=time_mean,aes(x=V1,y=V2,fill=V3)) + scale_fill_viridis(option="viridis",limits = c(0,1)) + labs(x = "On", y = "Off") + ggtitle("Mean time")
-pt_std <- p + geom_raster(data=time_std,aes(x=V1,y=V2,fill=V3)) + scale_fill_viridis(option="viridis") + labs(x = "On", y = "Off") + ggtitle("SD time")
-
-
-q <- ggplot() + theme(plot.margin = unit(c(0, 0, 1.25, 0), "cm"),legend.title=element_blank()) + scale_x_continuous(expand=c(0,0)) + scale_y_continuous(expand=c(0,0)) + guides(fill = guide_colorbar(title.hjust = 0.5, title.position = "top"))
-save_plot(plot_grid(p_mean,p_std,pt_mean,pt_std,ncol=2),filename="~/Documents/Notre Dame/ND paper 2/Code/heatmap_k20.png",base_height = 8,base_width = 8)
+save_plot(p,filename="~/Documents/Notre Dame/ND paper 2/Code/heatmap_k10.png",base_height = 6,base_width = 6)
 """
 
 
 
-
+#
+#
+# num_sims = 200
+# control = 1 # initial input rate of bacteria
+# time = 2000 # length of process
+# X₀ = 1e3 # initial number of susceptible, n₁
+#
+# # Stochastic LV chemical reaction system
+# # Parameters, times are per hour
+#
+# # Parameters
+# b₁ = 0.2 # susceptible birth rate
+# d₁ = 0.18 # susceptible death rate
+# b₂ = 0.195 # resistant birth rate
+# d₂ = 0.18 # resistant death rate
+# γ₁₁ = 1e-4 # susceptible death rate from other susceptible
+# γ₁₂ = 0.1e-4 # susceptible death rate from resistant
+# γ₂₁ = 10e-4 # resistant death rate from susceptible
+# γ₂₂ = 1e-4 # resistant death rate from other resistant
+# α₁ = 0.04 # susceptible death rate from antibiotic
+# α₂ = 0.0 # resistant death rate from antibiotic
+# μ = 2e-5 # mutation rate
+# μₐ = 100*μ # mutation rate cause by antibiotic
+# η = 0 #4e-3 # pasmid transfer rate
+# init_pop = 1e3 #initial population size
+#
+# β₁ = 0.2 # susceptible birth rate
+# δ₁ = 0.18 # susceptible death rate
+# β₂ = 0.1 # resistant birth rate
+# δ₂ = 0.09 # resistant death rate
+# γ₁₁ = 1e-5 # susceptible death rate from other susceptible
+# γ₂₂ = 1e-5 # resistant death rate from other resistant
+# α₁ = 0.03 # susceptible death rate from antibiotic
+# α₂ = 0.0 # resistant death rate from antibiotic
+# μ = 2e-5 # mutation rate
+# η = 0.0 # 4e-3 # pasmid transfer rate
+#
+#
+#
+# # b₁ = 0.02 # susceptible birth rate
+# # d₁ = 0.014 # susceptible death rate
+# # b₂ = 0.01 # resistant birth rate
+# # d₂ = 0.005 # resistant death rate
+# # γ₁₁ = 1e-6 # susceptible death rate from other susceptible
+# # γ₁₂ = 1e-6 # susceptible death rate from resistant
+# # γ₂₁ = 1e-6 # resistant death rate from susceptible
+# # γ₂₂ = 1e-6 # resistant death rate from other resistant
+# # α₁ = 0.008 # susceptible death rate from antibiotic
+# # α₂ = 0.0015 # resistant death rate from antibiotic
+# # μ = 1e-6 # mutation rate
+# # μₐ = 10*μ # mutation rate cause by antibiotic
+# # η = 0 #4e-3 # pasmid transfer rate
+# # init_pop = 1e4 #initial population size
+#
+# # Initial conditions
+# antibiotic_amount = 1.0 # amount of antibiotic when on
+# # durOn = 700 # durtation the antibiotic is on
+# # durOff = 1500 # duration the antibiotic is off
+#
+# # b₁ = 0.04
+# # d₁ = 0.03
+# # b₂ = 0.03
+# # d₂ = 0.02
+# # γ₁₁ = 1e-4
+# # γ₁₂ = 0.88e-4
+# # γ₂₁ = 1.136e-4
+# # γ₂₂ = 1e-4
+# # α₁ = 0.0012
+# # α₂ = 0
+# # μ = 1.5e-8
+# # ζ = 0.0
+# # η = 0 #4e-3
+# # controlrate = 0
+# rates = (b₁, d₁, b₂, d₂, γ₁₁, γ₁₂, γ₂₁, γ₂₂, α₁, α₂, μ, μₐ, η)
+# LV_model = @reaction_network begin
+#     b₁, n₁ → 2n₁ # n₁ birth
+#     d₁, n₁ → 0 # n₁ death
+# 	b₂, n₂ → 2n₂ # n₂ birth
+# 	d₂, n₂ → 0 # n₂ death
+# 	γ₁₁, n₁ + n₁ → 0 # self competition of n₁
+# 	a*γ₂₁+(1-a)*γ₁₂, n₁ + n₂ → n₂ # competition to n₁ from n₂
+# 	a*γ₁₂+(1-a)*γ₂₁, n₂ + n₁ → n₁ # competition to n₂ from n₁
+# 	γ₂₂, n₂ + n₂ → 0 # self competition of n₂
+# 	α₁, a + n₁ → a # death by antibiotic
+# 	α₂, a + n₂ → a # death by antibiotic, α₂ < α₁
+# 	μ, n₁ → n₂ # mutation
+# 	μ, n₂ → n₁ # mutation
+# 	μₐ, a + n₁ → a + n₂ # mutation due to stress from the antibiotic
+# 	η, n₁ + n₂ → 2n₂ # plasmid (resistance) transfer
+# end b₁ d₁ b₂ d₂ γ₁₁ γ₁₂ γ₂₁ γ₂₂ α₁ α₂ μ μₐ η
+#
 
 
 
